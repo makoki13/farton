a453ebcc67517c4a3b494e22e457e5eaf5c554ee
makoki13 <pablo.makoki@gmail.com>
2022-04-19 17:31:49 +0200

    Genesis 1,1

---
+++ ton.dart
@@ -0,0 +1,14 @@
+library farton;
+
+enum Types {
+  ordinary,
+  pruned_branch,
+  library_reference,
+  merkle_proof,
+  merkle_update
+}
+enum Flavors { builder, slice, continuation }
+
+class SmartContract {}
+
+class Cell {}
